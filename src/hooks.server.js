1b0d9aeffe150f5884afdc2b8d2a5dfa11b4e664
ronvoluted <5785323+ronvoluted@users.noreply.github.com>
2022-09-15 11:51:15 +0200

    1 file modified

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,6 +1,6 @@
 /** @type {import('@sveltejs/kit').Handle} */
 export const handle = async ({ event, resolve }) => {
-	if (event.url.pathname === 'potato') {
+	if (event.url.pathname === '/potato') {
 		return Response.redirect(event.url.origin, 303);
 	}
 
